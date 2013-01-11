c040502b43fda528b890d3911daa8fba5ca7dd07
rothj1459 <rothj1459@gmail.com>
2013-01-11 16:06:10 +0000

    added the file for what will be the flatificator

---
+++ game/LuaRules/Gadgets/tp_flatificator.lua
@@ -0,0 +1,4 @@
+--we are going to put flatification code here
+--something like this:
+--  local unitx,unity,unitz = Spring.GetUnitPosition(unitID)
+--  Spring.LevelHeightMap(unitx-100,unitz-100,unitx+100,unitz+100,unity)
