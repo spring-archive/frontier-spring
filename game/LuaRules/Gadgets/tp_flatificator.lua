7eef75db6e3d4d1adecdb592984ea4b9fb358c66
rothj1459 <rothj1459@gmail.com>
2013-01-16 15:51:22 +0000

    tried to make flatificator work. still needs to be fixed.

--- game/LuaRules/Gadgets/tp_flatificator.lua
+++ game/LuaRules/Gadgets/tp_flatificator.lua
@@ -2,3 +2,24 @@
 --something like this:
 --  local unitx,unity,unitz = Spring.GetUnitPosition(unitID)
 --  Spring.LevelHeightMap(unitx-100,unitz-100,unitx+100,unitz+100,unity)
+
+function unitName (unitID)
+
+    if (not Spring.ValidUnitID (unitID)) then return "!invalid unitID in unitName()!" end
+
+    local udID =Spring.GetUnitDefID(unitID)
+
+    local uDef = UnitDefs [udID]
+
+    return uDef.name
+
+end
+
+--[[
+function gadget:UnitFinished(unitID, unitDefID, teamID)
+	if(unitName(unitID)=="mnanoforge") then --test
+		local unitx,unity,unitz = Spring.GetUnitPosition(unitID)
+        Spring.LevelHeightMap(unitx-10000,unitz-10000,unitx+10000,unitz+10000,unity)
+	end
+end
+]]
